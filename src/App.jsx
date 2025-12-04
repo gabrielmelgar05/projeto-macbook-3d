d9586d1f7c7db2c598ec5feffd9c767f3b1f90d7
gabrielmelgar05 <gabrielmelgar.digital@gmail.com>
2025-12-04 09:57:49 -0400

    Creating the Showcase animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import ProductViewer from "./components/ProductViewer"
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import ProductViewer from "./components/ProductViewer";
+import Showcase from "./components/Showcase";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <ProductViewer />
+      <Showcase />
     </main>
   )
 }
